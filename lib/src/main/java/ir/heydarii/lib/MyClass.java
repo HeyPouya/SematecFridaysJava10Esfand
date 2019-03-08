dc4331bcc700ae4b68be949cc5b718f9ca0ec8ab
Pouya <pouyaheydary@gmail.com>
2019-03-08 12:02:25 +0330

    Initial commit

---
+++ lib/src/main/java/ir/heydarii/lib/MyClass.java
@@ -0,0 +1,76 @@
+package ir.heydarii.lib;
+
+/*
+This class is a test class to learn java's functionallities
+ */
+public class MyClass {
+
+    //assigning a number
+    static double P_NUMBER = 3.14;
+    static String my = "hi";
+
+    public static void main(String[] args) {
+        String name = getName(1);
+
+        System.out.println(name);
+        System.out.println(name);
+
+
+    }
+
+
+    /**
+     * this
+     *
+     *
+     * @param a to get the user's birthYear
+     * @return user's name
+     */
+    public static String getName(int a) {
+        String s = "Pouya";
+        return s;
+    }
+
+
+    //      { }     curly brace
+
+
+    //      [       brace
+
+
+    //      #       sharp
+
+
+    //      /       slash
+
+
+    //      \       back slash
+
+
+    //      |       pipe line
+
+
+    //      -       dash
+
+    //      _       under line
+
+    //      ( )     parantez
+
+    //      &       ampersand
+
+    //      *       star
+
+    //      :       colon
+
+    //      ;       semi - colon
+
+    //      '       single quotation
+
+    //      "       double quotation
+
+    //      ,       comma
+
+    //      $       dollar sign
+
+
+}
